8822f39e222caf80a2cc5930ca6cbad136d39f73
Abdulla A. B. <babulwriting@gmail.com>
2026-03-29 10:10:08 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// Add click effect to cards
+document.querySelectorAll('.course-card').forEach(card => {
+  card.addEventListener('click', () => {
+    if (card.querySelector('a').getAttribute('href') !== '#') {
+      // You can add more JS here later
+    }
+  });
+});
 
+// Simple welcome alert (remove later if you don't want it)
+console.log('%c✅ Course site loaded successfully!', 'color: #1a73e8; font-size: 16px;');
